fae1483755b25e4ab3cfc73d7084705e8a37f8c6
Ashnuta <ashnuta.0401@gmail.com>
2023-07-07 12:12:17 +0530

    javascript

---
+++ Javascript/index.js
@@ -0,0 +1,86 @@
+ //line below is used to print on the console window
+//  console.log('Namaste Dunia Version4');
+
+ //variables creation
+//  let a='true';
+//  console.log(a);
+
+ //constant
+//  const num=12;
+//  num=13;
+
+
+//dynamic typing
+// let uName = 'Ashnu';
+// console.log(uName);    //since only name is deprecated(obsolete)
+
+// uName = 4;
+// console.log(uName);
+
+//operators
+// let a=12;
+// let b=5;
+// console.log(a%b);
+// console.log(a>b);
+// console.log(a === b);
+// console.log(5 === 5);
+
+//conditional or ternary operator
+// let age=17;
+// let status = (age>=18)? 'Yes vote' : 'No vote';
+// console.log(status);
+
+//condition statements
+//if elseif else
+
+// let marks = 68;
+// if(marks >= 90){
+//     console.log('A');
+// }
+// else if(marks >= 80){
+//     console.log('B');
+// }
+// else if(marks >= 70){
+//     console.log('C') ;
+// }
+// else if(marks >= 60){
+//     console.log('D') ;
+// }
+// else{
+//     console.log('E') ;
+// }
+
+//switch case
+
+// let num=2;
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default:console.log('D');
+// }
+
+//for loop
+
+for(let i=0; i<5; i=i+1){
+    // console.log(i);
+    console.log("Ashnu" + i);
+}
+
+//while loop
+let x=1;
+
+while(x<=5){
+    console.log(x);
+    x++;
+}
+
+let y=1;
+
+do{
+    console.log(y);
+    y++;
+}while(y<10);
